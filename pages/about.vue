d692f4145154331b8c82088a50812aeeee55f783
Horace <horace.keung@incrowdsports.com>
2018-02-08 09:57:50 +0000

    update template

--- pages/about.vue
+++ pages/about.vue
@@ -5,6 +5,7 @@
 		<p>This template is based on <a href="https://nuxtjs.org/guide/installation" target="_blank">nuxt starter template</a> and <a href="https://github.com/nuxt-community/electron-template" target="_blank">electron template</a>.</p>
 		<p>Vuex persist functionality base on <a href="https://www.npmjs.com/package/vuex-persistfile" target="_blank">vuex-persistfile</a>.</p>
 		<p><a href="https://getbootstrap.com/" target="_blank">Bootstrap 4</a> is included.</p>
+		<p><a href="https://github.com/electron-userland/electron-builder" target="_blank">Electron builder</a> is included to build a ready for distribution Electron app.</p>
 	</section>
 </template>
 
